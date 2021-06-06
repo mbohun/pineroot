2a2acf71d538681a6006b9c5c9a2a3b365895cb4
Mika Penttilä <mika.penttila@gmail.com>
2021-06-06 14:20:59 +0300

    boot logo fixes

--- buildroot-2021.02.2/board/pine64/pine64/boot.cmd
+++ buildroot-2021.02.2/board/pine64/pine64/boot.cmd
@@ -1,4 +1,4 @@
-setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p2 rootwait
+setenv bootargs console=tty0 console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p2 rootwait
 
 fatload mmc 0 $kernel_addr_r Image
 fatload mmc 0 $fdt_addr_r sun50i-a64-pinephone-1.2.dtb
