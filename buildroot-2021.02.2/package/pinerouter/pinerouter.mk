24639020573934c8040baec3f77eea5155a87cb9
Pasi Patama <pasi@patama.net>
2021-06-19 04:50:14 +0300

    Corrected pinerouter package

--- buildroot-2021.02.2/package/pinerouter/pinerouter.mk
+++ buildroot-2021.02.2/package/pinerouter/pinerouter.mk
@@ -1,5 +1,5 @@
-PINEROUTER_VERSION = 8bcaf57c6f9b9632d5e227a0b409513cf352cc56
-PINEROUTER_SITE = https://github.com/e59e4835/pinerouter.git
+PINEROUTER_VERSION = 8b339eecc4f5dc31fd8ff9424c34fd9d4a5b4c02
+PINEROUTER_SITE = https://github.com/e59e4835/pinerouter-ui.git
 PINEROUTER_SITE_METHOD = git
 PINEROUTER_DEPENDENCIES = qt5base qt5xmlpatterns
 PINEROUTER_PREFIX = $(TARGET_DIR)/usr
